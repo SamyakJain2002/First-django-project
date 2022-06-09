587465ba61c3082104e1dbd1d0e3a272a666b01a
Samyak Jain <jainsamyak.com@gmail.com>
2022-06-09 19:42:20 +0530

    Done till password reset

--- django_project/settings.py
+++ django_project/settings.py
@@ -133,3 +133,10 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 LOGIN_REDIRECT_URL = 'blog-home'          #to redirect the user to home page after logging in
 LOGIN_URL = 'login'                       #to change the default login page to our login template
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.environ.get('EMAIL_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASS')
